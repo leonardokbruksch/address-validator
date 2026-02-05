508ea88bd7d52d3cc209170cadd0cb91c80fa8b1
leonardo <leonardokbruksch@hotmail.com>
2026-02-05 16:15:44 -0300

    run linting on nestjs

--- apps/api/src/lambda.ts
+++ apps/api/src/lambda.ts
@@ -1,27 +1,27 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ExpressAdapter } from "@nestjs/platform-express";
 import serverlessExpress from "@codegenie/serverless-express";
-import type { Handler, Context, Callback } from "aws-lambda";
+import { NestFactory } from "@nestjs/core";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import type { Callback, Context, Handler } from "aws-lambda";
 import express from "express";
+import { AppModule } from "./app.module";
 
 let server: Handler;
 
 async function bootstrap() {
-  const expressApp = express();
-  const adapter = new ExpressAdapter(expressApp);
+	const expressApp = express();
+	const adapter = new ExpressAdapter(expressApp);
 
-  const app = await NestFactory.create(AppModule, adapter);
-  await app.init();
+	const app = await NestFactory.create(AppModule, adapter);
+	await app.init();
 
-  return serverlessExpress({ app: expressApp });
+	return serverlessExpress({ app: expressApp });
 }
 
 export const handler: Handler = async (
-  event: unknown,
-  context: Context,
-  callback: Callback,
+	event: unknown,
+	context: Context,
+	callback: Callback,
 ) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
+	server = server ?? (await bootstrap());
+	return server(event, context, callback);
 };
