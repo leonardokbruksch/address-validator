cc2cb5d6dfe2f89b2d0296d3da81df8aec7b53d2
leonardo <leonardokbruksch@hotmail.com>
2026-02-05 15:42:20 -0300

    clean up boilerplate react + vite template

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// biome-ignore lint/style/noNonNullAssertion: The root element is guaranteed to exist in this context.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
